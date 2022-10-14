7a8e06f481bfafdcfb8f6f706dc327adfa5a45e4
Nomena <n.raminoheritiana@bolzanogroup.com>
2022-10-14 14:26:48 +0300

    init vuejs2, vue-router, vuex + tests manipulation, creation store

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+import Routes from './routes'
+import {store} from "./store";
 
-Vue.config.productionTip = false
+// Use packages
+Vue.use(VueRouter);
+
+// Register routes
+const router = new VueRouter({
+    routes: Routes,
+    mode: 'history'
+});
 
 new Vue({
+  el: '#app',
   render: h => h(App),
-}).$mount('#app')
+  router: router,
+  store
+})
---
+++ src/routes.js
@@ -0,0 +1,8 @@
+import Blog from "./components/Blog.vue";
+import RegisterUser from "./components/RegisterUser.vue";
+
+
+export default [
+    { path: '/', component: Blog},
+    { path: '/user_register', component: RegisterUser},
+]
